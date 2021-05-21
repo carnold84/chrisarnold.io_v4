533d69c9081c7bd2b450fbc113eac5b47e189e47
c-arnold <chris.wittyname@gmail.com>
2021-05-21 18:46:27 +1200

    Added Google analytics

--- nuxt.config.js
+++ nuxt.config.js
@@ -35,7 +35,7 @@ export default {
   css: [],
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
-  plugins: ['~/plugins/vue-simple-portal'],
+  plugins: ['~/plugins/vue-Gtag', '~/plugins/vue-simple-portal'],
 
   // Auto import components (https://go.nuxtjs.dev/config-components)
   components: true,
@@ -58,9 +58,9 @@ export default {
 
   // Build Configuration (https://go.nuxtjs.dev/config-build)
   build: {
-    analyze: {
+    /* analyze: {
       analyzerMode: 'static',
-    },
+    }, */
   },
 
   router: {
